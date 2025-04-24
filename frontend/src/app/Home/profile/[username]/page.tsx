b4cf3e8d79c4ac7ee919b434582a4743e210e403
amurlin <e.amurlin@gmail.com>
2025-04-24 11:48:52 +0800

    zasvar profile page

--- frontend/src/app/Home/profile/[username]/page.tsx
+++ frontend/src/app/Home/profile/[username]/page.tsx
@@ -89,7 +89,7 @@ export default function ProfilePage( ) {
                     <button
                       className="bg-black text-white px-4 py-2 rounded-md"
                       onClick={() => {
-                        // Highlight name-ыг хадгалах logic энд бичигдэнэ
+                        // Highlight creation logic here
                         setShowHighlightModal(false);
                       }}
                     >
