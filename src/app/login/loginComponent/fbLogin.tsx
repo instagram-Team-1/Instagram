e35475db0f46efd57232a1ebc30d69c54fe3f492
xop141 <lowsens400@gmail.com>
2025-04-27 23:45:37 +0800

    asd (#89)

--- src/app/login/loginComponent/fbLogin.tsx
+++ src/app/login/loginComponent/fbLogin.tsx
@@ -4,7 +4,7 @@ import { Facebook } from "lucide-react";
 const fbLogin = () => {
   return (
   
-       <Button variant="ghost" className="text-blue-500 flex items-center gap-2 ">
+       <Button variant="ghost" className="text-white flex items-center gap-2 bg-blue-500 w-full">
       <Facebook className="w-5 h-5 rounded-full" />
       <span>Login with Facebook</span>
     </Button>
