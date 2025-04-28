ecf3f376568ec4e535d2cfffdc5ddc0832422be2
Enkhjargal-hub <Joloenja@gmail.com>
2025-04-28 10:50:43 +0800

    home

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -6,13 +6,12 @@ import authRouter from "../src/routers/authRoute";
 import PostRouter from "./routers/PostRouter";
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 9000;
 dotenv.config();
 
 app.use(express.json());
-app.use(cors());
-
-
+// app.use(cors());
+app.use(cors({ origin: "http://localhost:3000" }));
 
 const mongoConnectionString = process.env.MONGO_CONNECTION_STRING;
 
