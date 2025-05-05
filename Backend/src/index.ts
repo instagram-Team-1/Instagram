3d53e4e2a3e724b697e19e259b2a1c962fc3d3ae
hfshsiflkd <toogiit0521@gmail.com>
2025-05-05 10:55:32 +0800

    cors aldaa

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -21,7 +21,14 @@ const app = express();
 const port = process.env.PORT || 9000;
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://instagramfrontendddd.vercel.app",
+    ],
+  })
+);
 
 const server = http.createServer(app);
 
