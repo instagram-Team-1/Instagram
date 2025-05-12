3b58725a09a5d73a0ab596ba815bdd68f4c3983d
hfshsiflkd <toogiit0521@gmail.com>
2025-05-12 18:55:57 +0800

    154 cdci tohirgoo (#164)
    * ci cd
    
    * Update api.tsx
    
    * jijig oorchllolt
    
    * Update index.ts
    
    * jijig aldaa
    
    * hello

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -30,15 +30,46 @@ export type FollowerType = {
   followers: string[];
 };
 
-export interface Story {
-  name: string;
-  img: string;
-  isLive?: boolean;
-  timestamp: string;
-}
+export type PostCardProps = {
+  imageUrl: string;
+  caption: string;
+  userId: {
+    posts: never[];
+    followers: number;
+    following: number;
+    _id: string;
+    username: string;
+    avatarImage: string;
+  };
+  likes: number;
+  comments: {
+    userId: string;
+    comment: string;
+    createdAt: string;
+    _id: string;
+  }[];
+  postId: string;
+  currentUserId: string;
+  currentUserUsername: string;
+};
 
-export interface HighlightType {
-  id: string;
-  title: string;
-  // stories: Story[];
-}
+export type Post = {
+  imageUrl: string;
+  _id: string;
+  image: string;
+  caption: string;
+  userId: {
+    _id: string;
+    username: string;
+    avatarImage: string;
+  };
+  likes: number | string;
+  comments: {
+    userId: string;
+    comment: string;
+    createdAt: string;
+    _id: string;
+  }[];
+  createdAt: string;
+  updatedAt: string;
+};
