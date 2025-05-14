67c2106c11b30eae4a77bd82ada433f79df47a1c
hfshsiflkd <toogiit0521@gmail.com>
2025-05-14 10:52:32 +0800

    jijig zasvar

--- Backend/src/controller/POST/GetFollowingPost.ts
+++ Backend/src/controller/POST/GetFollowingPost.ts
@@ -8,17 +8,29 @@ export const getFeedPosts = async (req: Request, res: Response) => {
 
     const user = await User.findById(userId);
     if (!user) {
-      res.status(404).json({ message: "User not found" });
-      return;
+       res.status(404).json({ message: "User not found" });
+       return;
     }
-    const posts = await Post.find({ userId: { $in: user.following } })
-      .sort({ createdAt: -1 }) 
-      .populate("userId", "username avatarImage");
-    
-    res.status(200).json(posts);
-    return;
+
+    let posts;
+
+    // Хэрвээ хэрэглэгч хүн дагаагүй бол:
+    if (!user.following || user.following.length === 0) {
+      posts = await Post.find()
+        .sort({ likes: -1 }) // Лайкны дагуу эрэмбэлэх
+        .limit(5) // Зөвхөн хамгийн их лайктай 1 пост
+        .populate("userId", "username avatarImage");
+    } else {
+      // Хэрвээ дагаж буй хүмүүс байвал, тэдний постуудыг авах
+      posts = await Post.find({ userId: { $in: user.following } })
+        .sort({ createdAt: -1 })
+        .populate("userId", "username avatarImage");
+    }
+
+     res.status(200).json(posts);
+     return;
   } catch (err) {
-    res.status(500).json({ message: "Server error", error: err });
-    return;
+     res.status(500).json({ message: "Server error", error: err });
+     return;
   }
 };
