f242b2162e38392a2445c4d558dd222102c8ab26
hfshsiflkd <toogiit0521@gmail.com>
2025-05-15 13:29:52 +0800

    bas aldaa

--- frontend/src/app/Home/components/appsider/_components/NotificationButtonPanel.tsx
+++ frontend/src/app/Home/components/appsider/_components/NotificationButtonPanel.tsx
@@ -77,14 +77,6 @@ function NotificationButtonPanel({
     return notif.message || "New notification";
   };
 
-  if (!data) {
-    return (
-      <div className="flex items-center justify-center min-h-screen w-full">
-        <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
-      </div>
-    );
-  }
-
   return (
     <div>
       <div
