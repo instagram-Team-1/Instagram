87a2d5fa2f08f6874638f5888f690a8db32e3cb6
hfshsiflkd <toogiit0521@gmail.com>
2025-05-16 08:44:01 +0800

    suuliin update

--- frontend/src/components/PostCard/_components/PostCardHeader.tsx
+++ frontend/src/components/PostCard/_components/PostCardHeader.tsx
@@ -1,35 +1,29 @@
 "use client";
 
-import { FC, useState } from "react";
+import { FC } from "react";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { User } from "lucide-react";
 import { User as UserType } from "@/lib/types";
 
 interface PostHeaderProps {
-  user: UserType;
+  user?: UserType | null; 
 }
 
 const PostHeader: FC<PostHeaderProps> = ({ user }) => {
-
   return (
     <div className="flex items-center justify-between py-3 px-4">
       <div className="flex items-center gap-4">
-        <div
-          className="relative"
-        >
+        <div className="relative">
           <Avatar className="w-[32px] h-[32px]">
-            <AvatarImage src={user.avatarImage || "/img/default-avatar.png"} />
+            <AvatarImage src={user?.avatarImage || "/img/default-avatar.png"} />
             <AvatarFallback>
               <User />
             </AvatarFallback>
           </Avatar>
-          
         </div>
-        <div
-          className="relative inline-block"
-        >
-          <span className=" text-sm font-medium cursor-pointer">
-            {user.username || ""}
+        <div className="relative inline-block">
+          <span className="text-sm font-medium cursor-pointer">
+            {user?.username || "Unknown User"}
           </span>
         </div>
       </div>
