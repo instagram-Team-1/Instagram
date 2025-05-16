a2ebea0f94c5b3f7e3a279e5eb264d3d4e78485e
xop141 <lowsens400@gmail.com>
2025-05-16 09:36:07 +0800

    Update page.tsx
    nee

--- frontend/src/app/Home/message/page.tsx
+++ frontend/src/app/Home/message/page.tsx
@@ -6,8 +6,6 @@ import Msgs from '../actualRoom/component/msgs';
 const page = () => {
   return (
     <div className="flex flex-col md:flex-row items-center justify-center w-full bg-black min-h-screen p-4 md:p-8">
-        <Msgs/>
-
       <div className="flex flex-col items-center gap-4 text-center">
         <div className="rounded-full p-4 md:p-6 lg:p-8 border border-white w-[150px] h-[150px] md:w-[200px] md:h-[200px] flex items-center justify-center">
           <Send size={80} className="text-white md:text-[100px]" />
